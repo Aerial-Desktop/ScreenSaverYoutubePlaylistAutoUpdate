e81dbc7e7b24b6dff0355731fca6adfb62f6c73d
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-01-27 23:23:21 -0500

    standard scaffold for screensaver. defaults to objective_c

---
+++ YoutubePLAU/YoutubePLAUView.h
@@ -0,0 +1,13 @@
+//
+//  YoutubePLAUView.h
+//  YoutubePLAU
+//
+//  Created by md on 1/27/18.
+//  Copyright © 2018 md. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface YoutubePLAUView : ScreenSaverView
+
+@end
---
+++ YoutubePLAU/YoutubePLAUView.m
@@ -0,0 +1,52 @@
+//
+//  YoutubePLAUView.m
+//  YoutubePLAU
+//
+//  Created by md on 1/27/18.
+//  Copyright © 2018 md. All rights reserved.
+//
+
+#import "YoutubePLAUView.h"
+
+@implementation YoutubePLAUView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
